577861047c9b58efc8c6d6b86d9a094266e74c3d
Bodo Kaiser <i@bodokaiser.io>
2018-04-24 15:38:27 +0200

    added Ratio function

--- approx.go
+++ approx.go
@@ -1,7 +1,8 @@
 package approx
 
 func PartialQuotients(a, b uint) (f ContFrac) {
-	var dividend, divisor, remainder uint = 0, a, b
+	var dividend uint
+	var divisor, remainder uint = a, b
 
 	for remainder != 0 {
 		dividend = divisor
@@ -12,4 +13,24 @@ func PartialQuotients(a, b uint) (f ContFrac) {
 	}
 
 	return f
+}
+
+func Rational(a, b, kmax uint) (uint, uint) {
+	cf := ContFrac{}
+	p, q := cf.Convergent(-1)
+
+	var dividend uint
+	var divisor, remainder uint = a, b
+
+	for k := 0; q <= kmax && remainder != 0; k++ {
+		dividend = divisor
+		divisor = remainder
+
+		cf = append(cf, dividend/divisor)
+		remainder = dividend % divisor
+
+		p, q = cf.Convergent(k)
+	}
+
+	return p, q
 }
--- approx_test.go
+++ approx_test.go
@@ -9,3 +9,10 @@ import (
 func TestPartialQuotients(t *testing.T) {
 	assert.Equal(t, ContFrac{3, 4, 12, 4}, PartialQuotients(649, 200))
 }
+
+func TestRational(t *testing.T) {
+	p, q := Rational(649, 200, 200)
+
+	assert.Equal(t, uint(649), p)
+	assert.Equal(t, uint(200), q)
+}
