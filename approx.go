db0566c0f96a4b3e4fb3591f0f7602533570cd86
Bodo Kaiser <i@bodokaiser.io>
2018-04-24 16:34:18 +0200

    merged frac.go into approx.go

--- approx.go
+++ approx.go
@@ -1,58 +1,84 @@
 package approx
 
-import "math"
+import (
+	"math"
+)
 
-func PartialQuotients(a, b uint) (f ContFrac) {
-	var dividend uint
-	var divisor, remainder uint = a, b
+type ContFrac []uint
+
+func NewContFrac(x float64) (f ContFrac) {
+	var dividend float64
+	var divisor, remainder float64 = x, 1
 
 	for remainder != 0 {
 		dividend = divisor
 		divisor = remainder
 
-		f = append(f, dividend/divisor)
-		remainder = dividend % divisor
+		f = append(f, uint(math.Floor(dividend/divisor)))
+		remainder = math.Mod(dividend, divisor)
 	}
 
 	return f
 }
 
-func Rational(a, b, kmax uint) (uint, uint) {
-	cf := ContFrac{}
-	p, q := cf.Convergent(-1)
-
-	var dividend uint
-	var divisor, remainder uint = a, b
-
-	for k := 0; q <= kmax && remainder != 0; k++ {
-		dividend = divisor
-		divisor = remainder
-
-		cf = append(cf, dividend/divisor)
-		remainder = dividend % divisor
-
-		p, q = cf.Convergent(k)
+func (f ContFrac) Float() float64 {
+	if len(f) == 0 {
+		return 0
+	}
+	if len(f) == 1 {
+		return float64(f[0])
 	}
 
-	return p, q
+	return float64(f[0]) + 1/f[1:].Float()
 }
 
-func Rational2(r float64, kmax uint) (uint, uint) {
-	cf := ContFrac{}
-	p, q := cf.Convergent(-1)
+func (f ContFrac) Convergent(k int) (p uint, q uint) {
+	if k == -1 {
+		p, q = 1, 0
 
-	var dividend float64
-	var divisor, remainder float64 = r, 1
+		return
+	}
+	if k == 0 {
+		p, q = f[0], 1
 
-	for k := 0; q <= kmax && remainder != 0; k++ {
-		dividend = divisor
-		divisor = remainder
-
-		cf = append(cf, uint(dividend/divisor))
-		remainder = math.Mod(dividend, divisor)
-
-		p, q = cf.Convergent(k)
+		return
+	}
+	if k > len(f)-1 {
+		panic("convergent exceeds number of fractions")
 	}
 
-	return p, q
+	pp, pq := f.Convergent(k - 1)
+	ppp, ppq := f.Convergent(k - 2)
+
+	p = f[k]*pp + ppp
+	q = f[k]*pq + ppq
+
+	return
+}
+func (f ContFrac) Ratio() (uint, uint) {
+	return f.Convergent(len(f) - 1)
+}
+
+func (f ContFrac) RatioConstr(kmax uint) (p uint, q uint) {
+	k := 0
+
+	for q <= kmax {
+		k++
+
+		p, q = f.Convergent(k)
+	}
+
+	return f.Convergent(k - 1)
+}
+
+func (f ContFrac) RatioConstr2(kmax, hmax uint) (p uint, q uint) {
+	k := 0
+
+	for q <= kmax && p <= hmax {
+		k++
+
+		p, q = f.Convergent(k)
+	}
+
+	return f.Convergent(k - 1)
 }
